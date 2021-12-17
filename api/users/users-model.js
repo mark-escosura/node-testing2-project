cbe19532d040e0c09584b5068f3a19a0beda2c4d
mark-escosura <escosuramarkse@gmail.com>
2021-12-16 16:55:11 -0800

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,26 +1,33 @@
-const database = require('../../data/db-config.js');
+const database = require("../../data/db-config.js");
 
 const getAll = () => {
-  return database('users')
-} 
-const getById = () => {
-  
-} 
-const insert = () => {
-  
-} 
-const update = () => {
-  
-} 
+  return database("users");
+};
 
-const destroy = () => {
-  
-} 
+const getById = (user_id) => {
+  return database("users").where("user_id", user_id).first();
+};
+
+const create = (newUser) => {
+  return database ('users')
+    .insert(newUser)
+    .then(([id]) => {
+      return getById(id)
+    })
+};
+
+const update = (user_id) => {
+  return null;
+};
+
+const destroy = (user_id) => {
+  return null;
+};
 
 module.exports = {
-    getAll,
-    getById,
-    insert,
-    update,
-    destroy
-}
+  getAll,
+  getById,
+  create,
+  update,
+  destroy,
+};
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -10,4 +10,29 @@ router.get("/", (req, res, next) => {
     .catch(next);
 });
 
+// [GET] returns a users id
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const user = await User.getById(id);
+  try {
+    if (!id) {
+      next({ status: 400, message: "No such ID exist!" });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+// [POST] returns a newly created record of user
+router.post("/", async (req, res, next) => {
+    const user = await User.create(req.body)
+    try {
+        res.status(201).json(user)
+    } catch (err) {
+        next(err)
+    }
+});
+
 module.exports = router;
